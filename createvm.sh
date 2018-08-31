b251f35a808d180362e48e276c1800d2d30dc7e3
Yuriy Novostavskiy <yuriy@novostavskiy.kiev.ua>
2018-08-31 09:16:07 +0000

    slap shit together and deploy

--- createvm.sh
+++ createvm.sh
@@ -19,7 +19,8 @@ OS_VAR=ubuntu16.04
 USERNAME=jin
 #assuming that id_rsa.pub generated alredy
 #need to be updated to check and generate key if missed
-readonly SSH_KEY=$(cat "~${USERNAME}/.ssh/id_rsa.pub")	
+#readonly SSH_KEY=$(cat "~${USERNAME}/.ssh/id_rsa.pub")	
+SSH_KEY=$(cat /home/$USERNAME/.ssh/id_rsa.pub)
 IP_ADD_BASE=192.168.122.1
 BASE_DIR=/var/lib/libvirt
 BOOT_DIR=$BASE_DIR/boot
@@ -72,6 +73,8 @@ if [ $? -eq 0 ]; then
 		echo "    path: /etc/network/interfaces" >> $IMAGE_DIR/$VM_NAME/user-data	
 		echo "runcmd: " >> $IMAGE_DIR/$VM_NAME/user-data
 		echo "  - [ sudo, service, networking, restart ] " >> $IMAGE_DIR/$VM_NAME/user-data
+		echo "  - sudo echo " $IP_ADD_BASE$1 $VM_NAME  ">> /etc/hosts" >> $IMAGE_DIR/$VM_NAME/user-data
+                echo "  - sudo ifdown ens3 && sudo ifup ens3 " >> $IMAGE_DIR/$VM_NAME/user-data
 		cp $IMAGE_DIR/$VM_NAME/user-data ./
 		cp $IMAGE_DIR/$VM_NAME/meta-data ./
 		mkisofs -o  $IMAGE_DIR/$VM_NAME/$VM_NAME-cidata.iso -V cidata -J -r user-data meta-data
@@ -80,6 +83,7 @@ if [ $? -eq 0 ]; then
 		mv $IMAGE_DIR/$VM_NAME/$VM_NAME.new.image $IMAGE_DIR/$VM_NAME/$VM_NAME.img
 		virsh pool-create-as --name $VM_NAME --type dir --target $IMAGE_DIR/$VM_NAME/
 		virt-install --import --name $VM_NAME --memory $RAM --vcpus $VCPU --cpu host --disk $IMAGE_DIR/$VM_NAME/$VM_NAME.img,format=qcow2,bus=virtio --disk $IMAGE_DIR/$VM_NAME/$VM_NAME-cidata.iso,device=cdrom --network bridge=virbr0,model=virtio --os-type=linux --os-variant=$OS_VAR --graphics spice --noautoconsole
+sleep 180
 fi
 }
 
@@ -92,7 +96,7 @@ for i in 1 $Q_TY_of_VM
 do 
  mkdir -p $IMAGE_DIR/vm0$i
  createvm $i &>>/var/vm.err.log
-# sleep 90
+ #sleep 180 
 done 
 
 
